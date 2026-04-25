a4bec6659ec39df881be0a1180baf62b23caa4ea
Navinya-30 <navinyaghanekar30@gmail.com>
2026-04-25 12:58:44 +0530

    Day 3: Add src/extension.js with base module skeleton

---
+++ src/extension.js
@@ -0,0 +1,12 @@
+// ============================================================
+// AUTO COMMENT GENERATOR - VS Code Extension
+// Main entry point: extension.js
+// ============================================================
+
+const vscode = require('vscode');
+
+function activate(context) {}
+
+function deactivate() {}
+
+module.exports = { activate, deactivate };
